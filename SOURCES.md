196ff20e4037c550cd07bdf9ce79f8ee9f5a095c
Henning Lund-Hanssen <henning.lund-hanssen@knowit.no>
2018-06-25 22:06:25 +0200

    Added curried examples

--- SOURCES.md
+++ SOURCES.md
@@ -4,3 +4,4 @@ Code taken from these sources:
 - https://dzone.com/articles/partially-applied-functions-in-scala?fromrel=true
 - https://dzone.com/articles/partial-functions-in-scala
 - https://alvinalexander.com/scala/how-to-define-use-partial-functions-in-scala-syntax-examples
+- https://www.grokacademy.com/lessons/demystifying-currying-and-partial-function-application/
---
+++ src/main/scala/no/knowit/chapter/jvm/pmatcher/CurriedNextLevel.scala
@@ -0,0 +1,20 @@
+package no.knowit.chapter.jvm.pmatcher
+
+object CurriedNextLevel {
+  def main(args: Array[String]): Unit = {
+    println("CurriedNextLevel:")
+    val primedOnce = curriedLine(5)
+    val primedTwice = primedOnce(10)
+    val result1 = primedTwice(2)
+    val result2 = primedTwice(4)
+    println("curried function primed with 5: " + primedOnce)
+    println("curried function primed with 10: " + primedTwice)
+    println("final result with 2: " + result1)
+    println("final result with 4: " + result2)
+    println()
+  }
+
+  def line(a: Int, b: Int, x: Int): Int = a + x + b
+
+  def curriedLine: Int => Int => Int => Int = (line _).curried
+}
